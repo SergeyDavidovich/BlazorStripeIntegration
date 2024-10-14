13abe8bd4fa9b6f99dae803f843ccab21ee68887
Sergiusz Kriwobokow <writesd@hotmail.com>
2024-10-14 15:10:47 +0200

    Create the Payment Controller

---
+++ BlazorStripeIntegration/BlazorStripeIntegration/Controllers/PaymentController .cs
@@ -0,0 +1,79 @@
+﻿using BlazorStripeIntegration.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Stripe.Checkout;
+using Stripe;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorStripeIntegration.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : Controller
+    {
+        private readonly StripeSettings _stripeSettings;
+
+        public PaymentController(IOptions<StripeSettings> stripeSettings)
+        {
+            _stripeSettings = stripeSettings.Value;
+        }
+
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            ViewBag.PublishableKey = _stripeSettings.PublishableKey;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCheckoutSession()
+        {
+            var options = new SessionCreateOptions
+            {
+                PaymentMethodTypes = new List<string> { "card" },
+                LineItems = new List<SessionLineItemOptions>
+            {
+                new SessionLineItemOptions
+                {
+                    PriceData = new SessionLineItemPriceDataOptions
+                    {
+                        UnitAmount = 2000,
+                        Currency = "eur",
+                        ProductData = new SessionLineItemPriceDataProductDataOptions
+                        {
+                            Name = "Test Product",
+                        },
+                    },
+                    Quantity = 1,
+                },
+            },
+                Mode = "payment",
+                SuccessUrl = Url.Action("Success", "Payment", null, Request.Scheme),
+                CancelUrl = Url.Action("Cancel", "Payment", null, Request.Scheme),
+            };
+
+            try
+            {
+                var service = new SessionService();
+                Session session = await service.CreateAsync(options);
+                return Json(new { id = session.Id });
+            }
+            catch (StripeException e)
+            {
+                return BadRequest(new { error = e.Message });
+            }
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+        public IActionResult Cancel()
+        {
+            return View();
+        }
+    }
+}
